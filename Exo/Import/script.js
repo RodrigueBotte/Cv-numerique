18446f1e51a1c3cba71f458a7bcdcc6d4dbf9a0b
rodrigue botte <rodrigue.botte@gmail.com>
2024-03-25 16:23:17 +0100

    exo import

---
+++ Exo/Import/script.js
@@ -0,0 +1,7 @@
+'use strict'
+
+import {affiche} from "./slider.js"
+const images = ["./images/pays1.jpg", "./images/pays2.jpg", "./images/pays3.jpg", "./images/pays4.jpg", "./images/corentin.jpg"]
+
+affiche(images)
+
